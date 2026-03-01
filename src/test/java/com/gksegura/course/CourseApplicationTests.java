cd2395434cd0cdfa5ea9f9b0ea5b454ed67aeb51
José Segura <jose.segura.ms@gmail.com>
2026-03-01 02:42:21 -0300

    Project created

---
+++ src/test/java/com/gksegura/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gksegura.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
