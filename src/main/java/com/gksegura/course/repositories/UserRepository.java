47833adcc6091d6f14424a8ce31e1b24c129def1
José Segura <jose.segura.ms@gmail.com>
2026-03-02 21:33:55 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gksegura/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.gksegura.course.repositories;
+
+import com.gksegura.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
